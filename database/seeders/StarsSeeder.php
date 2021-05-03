14aecedca7a96846d1ab33ca849d6ade1074fe6b
Jocelyn DUQUESNOY <66518874+jduquesnoy@users.noreply.github.com>
2021-05-03 21:48:47 +0200

    seeding

---
+++ database/seeders/StarsSeeder.php
@@ -0,0 +1,29 @@
+<?php
+namespace Database\Seeders;
+use Faker\Factory as Faker;
+use Illuminate\Database\Seeder;
+
+class StarsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Creation of 20 models Star for exhibition thanks to PHPFAKER
+        for ($i = 0; $i < 20; $i++) {
+            $star = New \App\Models\Star();
+            $faker = Faker::create();
+            // $person = new Faker\Provider\en_US\Person($faker);
+            // $text = new Faker\Provider\Lorem($faker);
+            // $image = new Faker\Provider\Image($faker);
+            $star->firstname = $faker->firstName;
+            $star->lastname = $faker->lastName;
+            $star->description = $faker->text($maxNbChars = 350) ;
+            $star->photo = $faker->imageUrl($width=250, $height=250, 'people');
+            $star->save();
+        }
+    }
+}
