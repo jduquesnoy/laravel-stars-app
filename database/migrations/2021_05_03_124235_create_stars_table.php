5080925583c3da9f9f48066ae8f385e2f60308e8
Jocelyn DUQUESNOY <66518874+jduquesnoy@users.noreply.github.com>
2021-05-03 15:34:32 +0200

    creation of the database

---
+++ database/migrations/2021_05_03_124235_create_stars_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stars', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('firstname',32);
+            $table->string('lastname',32);
+            $table->text('description');
+            $table->string('photo',100);
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stars');
+    }
+}
